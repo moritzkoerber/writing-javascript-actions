3527c268c86373c23df812c71454e3509f032ea5
moritzkoerber <koerber.moritz@gmail.com>
2021-02-02 21:44:44 +0100

    Update

--- .github/actions/issue-maker/index.js
+++ .github/actions/issue-maker/index.js
@@ -12,8 +12,8 @@ async function run() {
         repo: github.context.repo.repo,
         owner: github.context.repo.owner,
         title: issueTitle,
-        body; jokeBody
-    }); 
+        body: jokeBody
+    });
   } catch (err) {
   core.setFailed(err.message);
 }
