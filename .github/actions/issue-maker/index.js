1e0d88c22425538d74b8b2e0c9f1c469c9f2324c
moritzkoerber <koerber.moritz@gmail.com>
2021-02-03 22:42:31 +0100

    Dddd

--- .github/actions/issue-maker/index.js
+++ .github/actions/issue-maker/index.js
@@ -7,7 +7,7 @@ async function run() {
     const jokeBody = core.getInput("joke");
     const token = core.getInput("repo-token");
     console.log(jokeBody);
-      console.log("this is a test");
+    console.log("this is a test");
     const octokit = github.getOctokit(token);
     const newIssue = await octokit.issues.create({
         repo: github.context.repo.repo,
